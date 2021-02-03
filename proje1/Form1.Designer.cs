34fe585fda2fa54f761b6d38dfdb71f40fd31d76
melisaakbaydar <51860635+melisaakbaydar@users.noreply.github.com>
2021-02-03 23:40:13 +0300

    Add project files.

---
+++ proje1/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace proje1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(280, 484);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(345, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(646, 484);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 502);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 502);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Remove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(345, 502);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Play";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(450, 502);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 40);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Pause";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(549, 502);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(672, 506);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Video Format";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(672, 532);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Audio Format";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormatString = "N2";
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(770, 499);
+            this.comboBox1.MaximumSize = new System.Drawing.Size(50000, 0);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormatString = "N2";
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(770, 529);
+            this.comboBox2.MaximumSize = new System.Drawing.Size(50000, 0);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(176, 24);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(340, 552);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(415, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 60;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 545);
+            this.label3.MaximumSize = new System.Drawing.Size(100, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "00:00:00:00";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 1;
+            this.trackBar2.Location = new System.Drawing.Point(340, 587);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(415, 56);
+            this.trackBar2.TabIndex = 100;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            this.trackBar2.MouseLeave += new System.EventHandler(this.trackBar2_MouseLeave);
+            this.trackBar2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar2_MouseUp);
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(351, 611);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(10, 10);
+            this.label4.TabIndex = 101;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(859, 570);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 17);
+            this.label5.TabIndex = 102;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 644);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ proje1/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MPLATFORMLib;
+using System.Runtime.InteropServices;
+
+namespace proje1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+        MPlaylistClass myPlaylist = new MPlaylistClass();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            //Set and enable a preview,bunları her projede yaz
+            myPlaylist.PreviewWindowSet("", panel1.Handle.ToInt32());
+            myPlaylist.PreviewEnable("", 1, 1);
+            myPlaylist.PropsSet("loop", "false");
+            //Start mFile object
+            myPlaylist.ObjectStart(new object());
+            //Fill video formats
+            int nCount;
+            int nIndex;
+            string strFormat;
+            M_VID_PROPS vidProps;
+            comboBox1.Items.Clear();
+            //Get video format count
+            myPlaylist.FormatVideoGetCount(eMFormatType.eMFT_Convert, out nCount);
+            comboBox1.Enabled = nCount > 0;
+            if (nCount <= 0) return;
+            for (int i = 0; i < nCount; i++)
+            {
+                //Get format by index
+                myPlaylist.FormatVideoGetByIndex(eMFormatType.eMFT_Convert, i, out vidProps, out strFormat);
+                comboBox1.Items.Add(strFormat);
+            }
+            //Check if there is selected format
+            myPlaylist.FormatVideoGet(eMFormatType.eMFT_Convert, out vidProps, out nIndex, out strFormat);
+            comboBox1.SelectedIndex = nIndex > 0 ? nIndex : 0;
+            //Fill audio formats
+            M_AUD_PROPS audProps;
+            comboBox2.Items.Clear();
+            //Get video format count
+            myPlaylist.FormatAudioGetCount(eMFormatType.eMFT_Convert, out nCount);
+            comboBox2.Enabled = nCount > 0;
+            if (nCount <= 0) return;
+            for (int i = 0; i < nCount; i++)
+            {
+                //Get format by index
+                myPlaylist.FormatAudioGetByIndex(eMFormatType.eMFT_Convert, i, out audProps, out strFormat);
+                comboBox2.Items.Add(strFormat);
+            }
+            //Check if there is selected format
+            myPlaylist.FormatAudioGet(eMFormatType.eMFT_Convert, out audProps, out nIndex, out strFormat);
+            comboBox2.SelectedIndex = nIndex > 0 ? nIndex : 0;
+            //set audio volume
+            trackBar1.Value = 50;
+            double dblPos = (double)trackBar1.Value / trackBar1.Maximum;
+            myPlaylist.PreviewAudioVolumeSet("", -1, -30 * (1 - dblPos));
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+           
+
+            int val1;
+            double val2;
+            double x;
+            double y;
+            double z;
+            if (listBox1.Items.Count > 0 && listBox1.SelectedIndex >= 0)
+            {
+
+                string strPathOrCommand;
+                MItem pItem;
+                double dblPos;
+                myPlaylist.PlaylistGetByIndex(listBox1.SelectedIndex, out dblPos, out strPathOrCommand, out pItem);
+
+                pItem.FileInOutGet(out x, out y, out z);
+               
+                double _dbFilePos;
+                //item ın position unu get ile alıp sonra ona set ediyoruz. böylece kaldıgı yerden devam edıyo
+                pItem.FilePosGet(out _dbFilePos);
+                pItem.FilePosSet(_dbFilePos, 0);
+                myPlaylist.FilePlayStart();
+
+                //myPlaylist.PlaylistPosSet(listBox1.SelectedIndex, 0, 0);
+                
+                myPlaylist.PlaylistGetCount(out val1, out val2);
+                label5.Text = "Video Time: " + val2.ToString() + "s";
+                
+                trackBar2.Maximum = Convert.ToInt32(z);
+               
+
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            myPlaylist.FilePlayPause(0);
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            myPlaylist.FilePlayStop(0);
+            myPlaylist.FilePosSet(0, 0);
+            trackBar2.Value = 0;
+            label4.Width = 10;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK && openFileDialog1.FileNames.Length != 0)
+            {
+                foreach (string t in openFileDialog1.FileNames)
+                {
+                    int nIndex = -1;
+                    MItem pFile;
+                    myPlaylist.PlaylistAdd(null, t, "", ref nIndex, out pFile);
+                }
+                updateList(); // this method allows to keep listBox1 in actual state.
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                myPlaylist.PlaylistRemoveByIndex(listBox1.SelectedIndex, 0);
+                updateList();
+            }
+
+        }
+        private void updateList()
+        {
+            listBox1.Items.Clear();
+            int nFiles;
+            double dblDuration;
+            myPlaylist.PlaylistGetCount(out nFiles, out dblDuration);
+            for (int i = 0; i < nFiles; i++)
+            {
+                string strPathOrCommand;
+                MItem pItem;
+                double dblPos;
+                myPlaylist.PlaylistGetByIndex(i, out dblPos, out strPathOrCommand, out pItem);
+                strPathOrCommand = strPathOrCommand.Substring(strPathOrCommand.LastIndexOf('\\') + 1);
+                listBox1.Items.Add(strPathOrCommand);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            double dblPos = (double)trackBar1.Value / trackBar1.Maximum;
+            myPlaylist.PreviewAudioVolumeSet("", -1, -30 * (1 - dblPos));
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            M_VID_PROPS vidProps = new M_VID_PROPS();
+            string strFormat;
+            myPlaylist.FormatVideoGetByIndex(eMFormatType.eMFT_Convert, comboBox1.SelectedIndex, out vidProps, out strFormat);
+            //Set new video format
+            myPlaylist.FormatVideoSet(eMFormatType.eMFT_Convert, ref vidProps);
+
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            M_AUD_PROPS audProps = new M_AUD_PROPS();
+            string strFormat;
+            myPlaylist.FormatAudioGetByIndex(eMFormatType.eMFT_Convert, comboBox2.SelectedIndex, out audProps, out strFormat);
+            //Set new audio format
+            myPlaylist.FormatAudioSet(eMFormatType.eMFT_Convert, ref audProps);
+
+        }
+        bool aa = false;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(aa)
+            {
+                return;
+            }
+            double a;
+            double b;
+            int c;
+            int d;
+            myPlaylist.PlaylistPosGet(out c, out d, out a, out b);
+            trackBar2.Value = Convert.ToInt32(a);
+            label4.Width = (trackBar2.Value * (trackBar2.Width - 20)) / trackBar2.Maximum;
+
+           
+            eMState eState;
+            if (myPlaylist != null)//null exception için try-catch e alınmış.
+            {
+                myPlaylist.ObjectStateGet(out eState);
+                try
+                {
+                    int nCount;
+                    double dblListLen;
+                    myPlaylist.PlaylistGetCount(out nCount, out dblListLen);
+
+                    if (nCount > 0)
+                    {
+                        int nCurFile, nNextFile = 0;
+                        double dblFilePos, dblListPos = 0;
+                        myPlaylist.PlaylistPosGet(out nCurFile, out nNextFile, out dblFilePos, out dblListPos);
+
+                        string strPath;
+                        MItem pItem;
+                        double dblListOffset;
+                        myPlaylist.PlaylistGetByIndex(nCurFile, out dblListOffset, out strPath, out pItem);
+
+                        double dblIn, dblOut, dblFileLen = 0;
+                        pItem.FileInOutGet(out dblIn, out dblOut, out dblFileLen);
+                        if (dblOut > dblIn)
+                            dblFileLen = dblOut;
+                      
+                        
+                        string strFile = strPath.Substring(strPath.LastIndexOf('\\') + 1);
+
+                        label3.Text = Dbl2PosStr(dblFilePos) + "/" + Dbl2PosStr(dblFileLen) + " " +
+                            eState.ToString() + " (" + (nCurFile + 1) + "/" + nCount + ") " +
+                            Dbl2PosStr(dblListPos) + "/" + Dbl2PosStr(dblListLen) + " " +
+                            "\r\n" + strFile + "\r\n" + strPath;
+
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(pItem);
+                    }
+
+                    GC.Collect();
+                }
+                catch
+                {
+
+                    label4.Width = 0;
+                    throw;
+                }
+                string Dbl2PosStr(double _dblPos)
+                {
+                    int nHour = (int)_dblPos / 3600;
+                    int nMinutes = ((int)_dblPos % 3600) / 60;
+                    int nSec = ((int)_dblPos % 60);
+                    _dblPos -= (int)_dblPos;
+                    int nMsec = (int)(_dblPos * 1000 + 0.5);
+
+
+                    string strRes = nHour.ToString("00") + ":" + nMinutes.ToString("00") + ":" + nSec.ToString("00") + "." + nMsec.ToString("000");
+                    return strRes;
+                }
+            }
+
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            aa = true;
+            int nCount;
+            double dblListLen;
+            myPlaylist.PlaylistGetCount(out nCount, out dblListLen);
+            if (nCount > 0)
+            {
+
+                myPlaylist.FilePlayPause(0);
+
+                label4.Width = (trackBar2.Value * (trackBar2.Width - 20)) / trackBar2.Maximum;
+
+                double dblIn = 0;
+                double dblOut = 0;
+                double dblDuration = 0;
+                string strPath;
+
+                MItem pItem;
+                double dblListOffset;
+                myPlaylist.PlaylistGetByIndex(listBox1.SelectedIndex, out dblListOffset, out strPath, out pItem);
+
+                pItem.FileInOutGet(out dblIn, out dblOut, out dblDuration);
+                dblDuration = (dblOut > dblIn ? dblOut : dblDuration) - dblIn;
+                double dblPos = totalDur + dblIn + dblDuration * (double)trackBar2.Value / (double)trackBar2.Maximum;
+                myPlaylist.FilePosSet(dblPos, 0);
+            
+            }
+            
+        }
+
+     
+
+        private void trackBar2_MouseLeave(object sender, EventArgs e)
+        {
+
+        }
+        double totalDur = 0;
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            double val1;
+            string val2;
+            MItem val3;
+
+            myPlaylist.PlaylistGetByIndex(listBox1.SelectedIndex, out val1, out val2, out val3);
+            myPlaylist.PlaylistPosSet(listBox1.SelectedIndex, 0, 0);
+            int nCurFile, nNextFile = 0;
+            double dblFilePos, dblListPos = 0;
+            myPlaylist.PlaylistPosGet(out nCurFile, out nNextFile, out dblFilePos, out dblListPos);
+            totalDur = dblListPos;
+            myPlaylist.FilePlayStop(0);
+            trackBar2.Value = 0;
+            label4.Width = 0;
+            double x;
+            double y;
+            double z;
+            val3.FileInOutGet(out x, out y, out z);
+            trackBar2.Maximum = Convert.ToInt32(z);
+        }
+
+
+        private void trackBar2_MouseUp(object sender, MouseEventArgs e)
+        {
+
+            myPlaylist.FilePlayStart();
+            aa = false;
+
+
+        }
+
+        
+    }
+}
+
